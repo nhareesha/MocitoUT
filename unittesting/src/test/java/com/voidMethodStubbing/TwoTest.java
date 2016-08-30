018a763955ec1e23c2fa96e539650564f1eae6c5
hareesha <hareesha1906@gmail.com>
2016-08-30 13:48:29 -0700

    Exception scenari test case for void method

--- unittesting/src/test/java/com/voidMethodStubbing/TwoTest.java
+++ unittesting/src/test/java/com/voidMethodStubbing/TwoTest.java
@@ -1,9 +1,11 @@
 package com.voidMethodStubbing;
 
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyObject;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,6 +35,23 @@ public class TwoTest {
 		//verify if method is called
 		verify(one).fun();
 	}
+	/**
+	 * Should throw exception test
+	 * @throws Exception
+	 */
+	@Test(expected=RuntimeException.class)
+	public void voidMethodTest_should_throw_Exception() throws Exception{
+		
+		//set expectation on vid method
+		doThrow(Exception.class).when(one).fun();
+		
+		//call actual method
+		two.callVoidMethod();
+		
+		//verify if method is called
+		verify(one).fun();
+	}
+
 
 	@After
 	public void tearDown() throws Exception {
